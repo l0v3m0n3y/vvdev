d481a44488e17e00cde9846d43179b2f114536be
DarkKnight <94906343+l0v3m0n3y@users.noreply.github.com>
2024-07-17 12:56:53 +0300

    Add files via upload

---
+++ vvdev.js
@@ -0,0 +1,12 @@
+//mobile api function
+class vvdev{
+    constructor(){
+        this.api = "https://iplookup.vvdev.ru"
+        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"}
+    }
+    async my_ip(){
+        let req=await fetch(`${this.api}/me`,{method:"GET",headers: this.headers});
+        return req.json();
+    }
+}
+module.exports = {vvdev};
